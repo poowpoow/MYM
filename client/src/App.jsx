9d995ab9371a5b0ece2bdb28cd2f1fc9e1861534
Paula Berglund <paulalinnea.berglund@gmail.com>
2016-06-12 11:10:45 +0200

    - Added normalize.css - Started on a structure for the less files. - Added som markup to App.jsx just to plan how to structure the modules. - Started on menu script yo update active menu.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,38 @@ export default class App extends Component {
 
   render() {
     return (
-        <div>
-	  Hello from App.jsx!! Paula här!
+      <div>
+          <nav>
+            <a href="#">Öppna/Stäng meny</a>
+            <ul>
+              <li></li>
+            </ul>
+          </nav>
+          <heading>
+  	       <h1>Vi är en demokratisk, ...</h1>
+          </heading>
+          <section>
+            <h2>Hur kan du hjälpa?</h2>
+            <button>Ge en gåva</button>
+            <button>Bli medlem</button>
+          </section>
+          <section>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae augue non massa condimentum ultricies. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse semper mi vel odio tristique, ut eleifend orci ultrices. Aliquam varius pellentesque ipsum sollicitudin hendrerit.</p>
+            <button>Lär dig mer om MYM</button>
+          </section>
+          <section>
+            <h2>Projekt: Ukerew</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae augue non massa condimentum ultricies. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse semper mi vel odio tristique, ut eleifend orci ultrices. Aliquam varius pellentesque ipsum sollicitudin hendrerit.</p>
+            <button>Läs mer om projektet</button>
+          </section>
+          <section>
+            <h2>MYM bilder</h2>
+            <p>Instagram</p>
+          </section>
+          <footer>
+            <h2>Kontakta oss</h2>
+            <p>KontaktInfo</p>
+          </footer>
         </div>
     );
   }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 import App from './App';
 import About from './About';
+//import MenuExample from './Menu';
 
 require("./style/app.less");
 
@@ -11,6 +12,5 @@ ReactDOM.render(
 	<Route path="/" component={App}/>
   <Route path="/about" component={About}/>
 	<Route path="/:filter" component={App}/>
-  </Router>
-  , document.getElementById('root')
+  </Router>, document.getElementById('root')
 );
